7cfd7af824b33b4535561da91e779caaa88a88a4
Alex Toma <52259857+A1exToma@users.noreply.github.com>
2019-11-25 23:59:51 +0200

    no message

--- src/frontendmyApp/frontendApp.java
+++ src/frontendmyApp/frontendApp.java
@@ -3,12 +3,18 @@ package frontendmyApp;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -23,6 +29,8 @@ public class frontendApp {
 	private JPanel topPanel;
 	private JPanel rightPanelCart;
 	private JButton btnCumparare;
+	private JLabel lblProduse;
+	private JLabel lblCantitate;
 
 	
 	public static void main(String[] args) {
@@ -72,13 +80,47 @@ public class frontendApp {
 		btnCumparare.setForeground(Color.DARK_GRAY);
 		btnCumparare.setFont(new Font("Arial Black", Font.BOLD, 14));
 		btnCumparare.setBackground(Color.CYAN);
-		btnCumparare.setBounds(20, 476, 250, 23);
+		btnCumparare.setBounds(20, 447, 250, 23);
 		rightPanelCart.add(btnCumparare);
+		
+		JList cartList = new JList();
+		cartList.setForeground(Color.BLACK);
+		cartList.setBackground(Color.LIGHT_GRAY);
+		cartList.setFont(new Font("Calibri", Font.BOLD, 18));
+		cartList.setBounds(20, 198, 144, 34);
+		rightPanelCart.add(cartList);
+		
+		JLabel lblCos = new JLabel("      Cos");
+		lblCos.setForeground(Color.LIGHT_GRAY);
+		lblCos.setBackground(Color.CYAN);
+		lblCos.setFont(new Font("Arial Black", Font.PLAIN, 16));
+		lblCos.setBounds(84, 87, 90, 34);
+		rightPanelCart.add(lblCos);
+		
+		
+		Integer[] comboBoxValue= {0,1,2,3,4,5,6,7,8,9,10};//value from ComboBox
+		JComboBox comboBoxCartPiece = new JComboBox(comboBoxValue);
+		comboBoxCartPiece.setBackground(Color.CYAN);
+		comboBoxCartPiece.setBounds(163, 198, 51, 34);
+		rightPanelCart.add(comboBoxCartPiece);
+		
+		lblProduse = new JLabel("Produs");
+		lblProduse.setForeground(Color.LIGHT_GRAY);
+		lblProduse.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblProduse.setBounds(20, 173, 65, 14);
+		rightPanelCart.add(lblProduse);
+		
+		lblCantitate = new JLabel("Cantitate");
+		lblCantitate.setForeground(Color.LIGHT_GRAY);
+		lblCantitate.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblCantitate.setBounds(163, 173, 58, 14);
+		rightPanelCart.add(lblCantitate);
+		
 		
 		String[]columnNames= {"Denumire produs","Cantitate disponibila","Pret "};
 		Object[][] listaProduses = new Object[43][3];
 		
-		try {
+		try {//se citesc datele din fisierul plante
 			Scanner read= new Scanner (new File("D:\\github\\myPlafarApp\\src\\frontendmyApp\\plante.txt"));
 			
 			String denumire,temp;
@@ -120,7 +162,7 @@ public class frontendApp {
 		};
 		
 		
-		JScrollPane scrollPane=new JScrollPane();
+		JScrollPane scrollPaneTable=new JScrollPane();
 		table = new JTable(listaProduses,columnNames);
 		table.setRowHeight(35);
 		table.setShowHorizontalLines(false);
@@ -133,10 +175,43 @@ public class frontendApp {
 		table.setBackground(Color.CYAN);
 		table.setModel(tableModel);//nonEditable cells
 		
-		scrollPane.add(table);
-		scrollPane.setViewportView(table);
-		scrollPane.setBounds(0, 65, 506, 496);
+		scrollPaneTable.add(table);
+		scrollPaneTable.setViewportView(table);
+		scrollPaneTable.setBounds(0, 65, 506, 496);
 		
-		backGRDPanel.add(scrollPane);
+		backGRDPanel.add(scrollPaneTable);
+
+		table.addMouseListener(new java.awt.event.MouseAdapter() {
+			 public void mouseClicked(java.awt.event.MouseEvent evt) {
+			    int row= table.rowAtPoint(evt.getPoint());
+			    int col= table.columnAtPoint(evt.getPoint());
+
+			    if (row >= 0 && col == 0) {
+			    	DefaultListModel productModel=new DefaultListModel();
+					productModel.addElement(listaProduses[row][col]);
+			    	cartList.setModel(productModel);
+
+			    
+			    }
+			    
+			 }
+			});
+		
+		btnCumparare.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent arg0) {
+				int cantitate=(int) comboBoxCartPiece.getSelectedItem();
+				
+				//int actualvalue=(int) listaProduses[row][1];
+				
+				//if(actualvalue>cantitate) {
+				//listaProduses[row][1]=actualvalue-cantitate;
+				//}
+			}
+			
+		});
+		
+
+		
 	}
 }
